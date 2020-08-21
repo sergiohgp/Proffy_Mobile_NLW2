ad5845d1d9c607025fa33b3817cc3a3d0847d117
Sergio Pereira <sergiohgp@gmail.com>
2020-08-21 01:27:49 -0400

    Tabs done

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ function Landing() {
         navigate('GiveClasses')
     }
 
+    function handleNavigateToStudyPages() {
+        navigate('Study')
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -30,7 +34,7 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>
                         Study
